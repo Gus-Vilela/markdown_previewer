0a2724944f694edb9c2ee1de54ed6dcc769f2d12
Gus-Vilela <gustavo.carvil@protonmail.com>
2023-04-24 22:48:11 -0300

    add initial text for the editor, codecamp tests

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import {useState} from 'react'
 import {marked} from 'marked'
 
 function App() {
-  const iniText = `<h1>Markdown Previewer</h1>
-<h2>A simple tool to convert markdown to HTML</h2>
-<a href="https://github.com/myusername/markdown-previewer">View the source code on GitHub</a>
-<p>This is a demo of how the markdown-previewer works.</p>
-<ul>
-  <li>Type some markdown in the Editor panel</li>
-  <li>See the HTML output in the Preview panel</li>
-  <li>Enjoy!</li>
-</ul>
-<pre><code>// This is a small friendly function that greets the user <br>
-function greet() { <br>
-  return 'Hello, Nice to meet you.'; <br>
-}  <br>
-// This is how you call the function <br>
-console.log(greet());</code></pre>
-<blockquote>The journey of a thousand miles begins with a single step. - Lao Tzu</blockquote>
-<img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React logo" width="300">
-<p>This is some <strong>bolded text</strong> to show you how it looks.</p>
-<p>This is some <em>italicized text</em> to show you how it looks.</p>`;
-
+  const iniText = "# Markdown Previewer \n\
+## A simple tool to convert markdown to HTML\n\
+View the source code on [GitHub](https://github.com/myusername/markdown-previewer)\n\
+### This is a demo of how the markdown-previewer works.\n\
+- Type some markdown in the Editor panel\n\
+- See the HTML output in the Preview panel\n\
+- Enjoy!\n\
+```\n\
+// This is a small friendly function that greets the user \n\
+function greet() {\n\
+  return 'Hello, Nice to meet you.'; \n\
+}\n\
+// This is how you call the function \n\
+console.log(greet());\n\
+```\n\
+And some inline `<div>code</div>` exemple.\n\
+> The journey of a thousand miles begins with a single step. - Lao Tzu \n\
+![React Logo](https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg)\n\
+This is some **bolded** text to show you how it looks. <br>\n\
+This is some _italicized_ text to show you how it looks.\n\ ";
 
 
   const [state, setState] = useState({text: iniText, preview: true, editor: true})
@@ -37,14 +37,14 @@ console.log(greet());</code></pre>
       {state.editor && <div className="editor--wrap">
         <div className='editor--nav'>
           <p className='boxtitle'>Editor</p>
-          <button onClick={handleEditorClick}></button>
+          <button onClick={handleEditorClick}>Maximize</button>
         </div>
         <textarea id='editor' onChange={(e) => setState({...state, text: (e.target.value)})}>{state.text}</textarea>
       </div>}
       {state.preview && <div className='preview--wrap'>
         <div className='editor--nav'>
           <p className='boxtitle'>Preview</p>
-          <button onClick={handlePreviewClick}></button>
+          <button onClick={handlePreviewClick}>Maximize</button>
         </div>
         <div id='preview' dangerouslySetInnerHTML={{__html: marked(state.text)}}></div>
       </div>}
